a33df94d8a08b1a3c6a23e547b49bf9d7eabc225
19PH83 <44884678+19PH83@users.noreply.github.com>
2019-06-11 18:53:51 -0300

    Add files via upload

---
+++ src/main/java/com/agendamento/repository/FilialRepository.java
@@ -0,0 +1,7 @@
+package com.agendamento.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.agendamento.entity.Filial;
+
+public interface FilialRepository extends CrudRepository<Filial, Long>{}
